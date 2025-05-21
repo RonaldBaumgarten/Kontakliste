db5631f2ba608103fbe23549286ba6c30d189713
Ronald Baumgarten <baumgartendj@gmail.com>
2025-05-21 19:58:15 +0200

    Add Datei.cs

---
+++ Datei.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakliste
+{
+    internal class Datei
+    {
+        private string datei = @"";
+        private FileStream stream;
+
+        public void schreibeCSV( List<Kontakt> liKon)
+        {
+            stream = 
+                new FileStream(datei, FileMode.Create, FileAccess.Write);
+            StreamWriter sr = new StreamWriter(stream);
+
+            foreach (Kontakt k in liKon)
+            {
+                sr.WriteLine(k.toCSV());
+            }
+
+            sr.Close();
+            stream.Close();
+
+        }
+        public List<Kontakt> liesCSV()
+        {
+            List<Kontakt> liKon = new List<Kontakt>();
+            string s;
+            string[] arrstr;
+            stream = 
+                new FileStream(datei, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(stream);
+
+            while( sr.Peek() != -1 )
+            {
+                s = sr.ReadLine();
+                arrstr = s.Split(';');
+                liKon.Add(
+                    new Kontakt(arrstr[0], arrstr[1],
+                                arrstr[2], arrstr[3], 
+                                arrstr[4], int.Parse(arrstr[5]),
+                                arrstr[6], arrstr[7] ) );
+            }
+
+            sr.Close();
+            stream.Close();
+
+            return liKon;
+        }
+    }
+}
